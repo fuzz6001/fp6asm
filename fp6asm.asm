d9588659470b2a38a443c372fca057c40329a811
fuzzball <github@fuckin.rash.jp>
2014-04-04 10:32:34 +0900

    comments

--- fp6asm.asm
+++ fp6asm.asm
@@ -6,32 +6,36 @@ ATTR_SIZE	equ	$0200
 VRAM_BASE	equ	$e200
 VRAM_SIZE	equ	$0200
 
+COL_PIPE_SHADOW	equ	$20	;pipe (shadow)
+COL_PIPE	equ	$21	;pipe (bright)
+COL_SPACE	equ	$22	;space (empty)
+
+ldf00		equ	$df00	;
+ldf01		equ	$df01	;
+ldf02		equ	$df02	;1b	rand
+ldf03		equ	$df03	;
+
+BAS_BX		equ	$df10	;1w	X
+BAS_BY		equ	$df12	;1w	Y
+BAS_NY		equ	$df14	;1w	new Y
+;		equ	$df16	;1b
+;		equ	$df17	;1b
+BAS_SC		equ	$df18	;8b	score
+BAS_SC_NUM	equ	8	;最大8桁
+
+pl_spd		equ	$df80	;1w	player speed
+pl_acc		equ	$df82	;1w	player acceleration
+scroll_counter	equ	$df84	;1b
+
 keep_fa27	equ	$dff0	;1w
 keep_fa2d	equ	$dff2	;1w
 counter_2ms	equ	$dff4	;1w
 jobno		equ	$dff6	;1b
-
-
-
-COL_WALL1	equ	$20	;暗い壁
-COL_WALL2	equ	$21	;明るい壁
-COL_SPACE	equ	$22	;壁は無い
-
-ldf00	equ	$df00	;
-ldf01	equ	$df01	;
-ldf02	equ	$df02	;1b	rand
-ldf03	equ	$df03	;
-
-BAS_BX	equ	$df10	;1b	X
-BAS_BY	equ	$df11	;1b	Y
-BAS_NY	equ	$df12	;1b	new Y
-;	equ	$df13	;1b
-;	equ	$df14	;1b
-;	equ	$df15	;1b
-;	equ	$df16	;1b
-;	equ	$df17	;1b
-BAS_SC	equ	$df18	;8b	score
-BAS_SC_NUM	equ	8	;最大8桁
+;		equ	$dff7
+stick_state	equ	$dffc	;1b
+stick_old	equ	$dffd	;1b
+stick_trigger	equ	$dffe	;1b
+stick_trigger_	equ	$dfff	;1b
 
 JOBNO_INIT		equ	0
 JOBNO_TITLE		equ	1
@@ -55,13 +59,14 @@ main_loop_sync:
 	jr	nz,sync_over
 	ld	a,' '
 sync_over:
-	ld	hl,VRAM_BASE+$1f;画面右上
+	ld	hl,VRAM_BASE+$1f	;right-top corner
 	ld	(hl),a
 
 	ld	hl,0
 	ld	(counter_2ms),hl
 
 
+	call	update_stick
 	call	job_main
 
 
@@ -122,16 +127,16 @@ str_press_space:
 init_game_screen:
 	call	clear_screen
 
-	;地面
+	;ground
 	ld	hl,ATTR_BASE+0+15*$20
 	ld	de,ATTR_BASE+1+15*$20
 	ld	bc,32-1
-	ld	a,COL_WALL2	;明るい壁
+	ld	a,COL_PIPE
 	ld	(hl),a
 	ldir
 
-	;ポッチリ
-	ld	a,COL_WALL1	;暗い壁
+	;marking
+	ld	a,COL_PIPE_SHADOW
 	ld	(ATTR_BASE+15+15*$20),a
 	ld	(ATTR_BASE+31+15*$20),a
 
@@ -139,22 +144,34 @@ init_game_screen:
 
 ;-----------------------------------------------------------------------
 job_title_wait:
-	call	$1061
-	bit	7,a
+	ld	a,(stick_trigger)
+	bit	7,a	;push space?
 	ret	z
 
-	;スタート!!
+	;START!!
 	call	init_game_screen
-	call	ld000;オリジナルのスクロール初期化
+	call	ld000;init scroll
 
-	ld	a,4
-	ld	(BAS_BX),a
-	ld	a,7
-	ld	(BAS_BY),a
+	ld	hl,4<<8
+	ld	(BAS_BX),hl
+	ld	hl,7<<8
+	ld	(BAS_BY),hl
 
+	;player
+	ld	hl,$ffc0;-0.25
+	ld	(pl_spd),hl
+	ld	hl,$0003
+	ld	(pl_acc),hl	;gravity
+
+	;scroll
+	xor	a
+	ld	(scroll_counter),a
+
+	;score
 	xor	a
 	ld	hl,BAS_SC
 	ld	de,BAS_SC+1
+	ld	(hl),a
 	ld	bc,BAS_SC_NUM-1
 	ldir
 
@@ -163,52 +180,134 @@ job_title_wait:
 ;-----------------------------------------------------------------------
 job_game:
 
-	ld	a,(BAS_BY)
+	;check tap
+	ld	a,(stick_trigger)
+	bit	7,a	;push space?
+	jr	z,job_game_notap
 
-	;移動処理
+	;上昇中のタップ
+	ld	de,$fff8;	;ちょっとだけ浮く
 
-	ld	(BAS_NY),a
+	ld	hl,(pl_spd)
+	ld	a,h
+	and	a
+	jp	m,job_game_tap		;上昇中はちょっとだけ浮く
+	jr	nz,job_game_tap_down
+	ld	a,(pl_spd+0)
+	cp	$10
+	jr	c,job_game_tap		;下降中も最初の方はちょっとだけしか浮かない
+job_game_tap_down:
 
-	;自キャラ消す
+	;ガッツリ下降中のタップ
+	ld	de,$ffa0;
+
+job_game_tap:
+	add	hl,de
+	ld	(pl_spd),hl
+
+job_game_notap:
+	;move player
+	;y += speed
+	ld	hl,(BAS_BY)
+	ex	de,hl
+	ld	hl,(pl_spd)
+	add	hl,de
+	ld	(BAS_NY),hl
+	;speed += acc
+	ld	hl,(pl_spd)
+	ex	de,hl
+	ld	hl,(pl_acc)
+	add	hl,de
+	ld	(pl_spd),hl
+
+	;limit/over
+	ld	hl,(BAS_NY)
+	ld	de,1<<8
+	and	a;cf=0
+	sbc	hl,de
+	jr	c,job_game_limit_set
+	;limit/under
+	ld	hl,(BAS_NY)
+	ld	de,15<<8
+	and	a;cf=0
+	sbc	hl,de
+	jr	z,job_game_limit_end
+	jr	c,job_game_limit_end
+job_game_limit_set:
+	ex	de,hl
+	ld	(BAS_NY),hl
+job_game_limit_end:
+
+	;clear player
 	ld	de,VRAM_BASE
-	ld	hl,(BAS_BX)
+	ld	a,(BAS_BX+1)	;int(BX)
+	ld	l,a
+	ld	a,(BAS_BY+1)	;int(BY)
+	ld	h,a
 	ld	a,' '
 	push	hl
 	call	put_char
 	pop	hl
-	;attrも
+	;attr
 	ld	de,ATTR_BASE
 	call	pos2vram
 	ld	a,COL_SPACE
 	ld	(hl),a
 
-	;自キャラ描く
+	;change player pattern
+	ld	b,'6'
+	ld	a,(pl_spd+1)
+	and	a
+	jp	m,set_player_pattern
+	ld	b,'9'
+	jr	nz,set_player_pattern
+	ld	a,(pl_spd+0)
+	cp	$40
+	jr	nc,set_player_pattern
+	ld	b,$e9;'の'
+set_player_pattern:
+
+	;draw player
 	ld	de,VRAM_BASE
-	ld	a,(BAS_NY)
-	ld	h,a
-	ld	a,(BAS_BX)
+	ld	a,(BAS_BX+1)	;int(BX)
 	ld	l,a
-	ld	a,'6'
+	ld	a,(BAS_NY+1)	;int(NY)
+	ld	h,a
+	ld	a,b;;'6'
 	push	hl
 	call	put_char
 	pop	hl
-	;attrも
+	;attr
 	ld	de,ATTR_BASE
 	call	pos2vram
 	ld	a,COL_SPACE
 	ld	(hl),a
 
-	;スクロール
+	;update pos
+	ld	hl,(BAS_NY)
+	ld	(BAS_BY),hl
+
+	;check scroll
+	ld	a,(scroll_counter)
+	inc	a
+	and	$07
+	ld	(scroll_counter),a
+	jr	nz,skip_scroll
+
+	;scroll
 	call	ld004
 
-	;アタリ判定
+	;dead or alive
 	ld	de,ATTR_BASE
-	ld	hl,(BAS_BX)
+	ld	a,(BAS_BX+1)	;int(BX)
+	ld	l,a
+	ld	a,(BAS_BY+1)	;int(BY)
+	ld	h,a
 	call	get_char
 	cp	COL_SPACE
 	jp	nz,job_next	;->gameover
 
-	;スコア
+	;score
 	ld	hl,str_score
 	ld	de,VRAM_BASE+12+0*$20
 	call	draw_text
@@ -216,6 +315,8 @@ job_game:
 	ld	de,VRAM_BASE+19+0*$20
 	call	draw_score
 	call	inc_score
+
+skip_scroll:
 
 	ret
 
@@ -231,11 +332,11 @@ inc_score_lp:
 	inc	a
 	ld	(hl),a
 	cp	10
-	ret	c	;繰り上がらないので終了
+	ret	c	;no carry -> finish
 
-	;桁上り
+	;carry over
 	sub	10
-	ld	(hl),a	;保存し直し
+	ld	(hl),a	;re-store
 	dec	hl
 	djnz	inc_score_lp
 	ret
@@ -304,8 +405,8 @@ str_replay:
 
 ;-----------------------------------------------------------------------
 job_over_wait:
-	call	$1061
-	bit	0,a
+	ld	a,(stick_trigger)
+	bit	0,a	;shift
 	ret	z
 
 	ld	a,JOBNO_TITLE
@@ -372,7 +473,7 @@ draw_score_skip:
 ;-----------------------------------------------------------------------
 ;-----------------------------------------------------------------------
 entry:
-	;スタック初期化/2msタイマーハンドラ登録
+	;init stack & register 2m-timer handler
 	di
 ;	ld	(keep_sp),sp
 ;	ld	hl,LOCAL_STACK
@@ -381,18 +482,19 @@ entry:
 	ld	($fa06),hl
 	ei
 
-	;ポート$b0保存
+	;keep port $b0
 	ld	a,($fa27)
 	ld	(keep_fa27),a
 
-	;キークリック音OFF
+	;turn off click sound
 	ld	a,($fa2d)
 	ld	(keep_fa2d),a
 	xor	a
 	ld	($fa2d),a
 
-	;画面初期化
+	;initialize devices
 	call	init_screen
+	call	init_stick
 
 	;メインループへ
 	ld	a,JOBNO_INIT
@@ -402,7 +504,7 @@ entry:
 	jp	main_loop
 
 ;-----------------------------------------------------------------------
-;	2msタイマーハンドラ
+;	2ms-timer handler
 ;-----------------------------------------------------------------------
 timer_handler:
 	push	hl
@@ -414,7 +516,7 @@ timer_handler:
 	ret
 
 ;-----------------------------------------------------------------------
-;	画面初期化
+;	initialize screen
 ;-----------------------------------------------------------------------
 init_screen:
 	;表示OFF
@@ -454,6 +556,35 @@ clear_screen:
 	ret
 ;-----------------------------------------------------------------------
 ;-----------------------------------------------------------------------
+init_stick:
+	xor	a
+	ld	(stick_state),a
+	call	update_stick
+	ret
+
+;-----------------------------------------------------------------------
+update_stick:
+	;update stick
+	ld	a,(stick_state)
+	ld	(stick_old),a
+	ld	b,a	;keep old
+	call	$1061
+	ld	(stick_state),a
+	ld	c,a	;keep state
+
+	;(state XOR old) AND state = trigger
+	xor	b	;state XOR old
+	and	c	;AND state
+	ld	(stick_trigger),a
+
+	;(state XOR old) AND old = ~trigger
+	ld	a,b
+	xor	c	;state XOR old
+	and	b	;AND old
+	ld	(stick_trigger_),a
+
+	ret
+
 ;-----------------------------------------------------------------------
 ;-----------------------------------------------------------------------
 ;=======================================================================
@@ -467,7 +598,7 @@ ld000:
 	ret
 
 ;-----------------------------------------------------------------------
-;	entry 2
+;	entry 2 / scroll
 ;-----------------------------------------------------------------------
 ld004:
 	call	ld00b	;アトリビュートエリアを左スクロール
@@ -498,7 +629,7 @@ ld013:
 	ret
 
 ;-----------------------------------------------------------------------
-;	右端に柱とか作る
+;	右端に土管を作る
 ;-----------------------------------------------------------------------
 ld021:
 	ld	a,(ldf00)
@@ -508,13 +639,13 @@ ld021:
 	jr	c,ld042
 
 	;9以上
-	ld	a,COL_WALL2	;明るい壁
+	ld	a,COL_PIPE
 	ld	(ldf03),a
 	ld	a,(ldf01)
 	inc	a
 	dec	a
 	jr	z,ld053
-	ld	a,COL_WALL1	;暗い壁
+	ld	a,COL_PIPE_SHADOW
 	ld	(ldf03),a
 	ld	a,(ldf01);※これ必要ない!!
 	jr	ld053
@@ -526,15 +657,15 @@ ld042:
 	ld	bc,$0020
 	ld	hl,ATTR_BASE+$1f	;1行目の右端
 ld04a:
-	ld	(hl),COL_SPACE	;壁が無い
+	ld	(hl),COL_SPACE	;何も無い
 	add	hl,bc
 	dec	a
 	jr	nz,ld04a
-	;最初の壁は明るい!
-	ld	(hl),COL_WALL2	;明るい壁
+	;土管の最初は明るい!
+	ld	(hl),COL_PIPE
 	ret
 
-	;(ldf03) = 柱の色
+	;(ldf03) = 土管の色
 ld053:
 	ld	a,(ldf02)	;rand
 	ld	bc,$0020
@@ -550,7 +681,7 @@ ld05c:
 
 	ld	a,$06
 ld068:
-	ld	(hl),$22	;穴の色
+	ld	(hl),COL_SPACE	;何もない
 	add	hl,bc
 	dec	a
 	jr	nz,ld068
